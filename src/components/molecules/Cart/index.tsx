533b7a80a70137989213e45b124e9107d8102de0
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-10-23 14:35:46 -0300

    layout finished

---
+++ src/components/molecules/Cart/index.tsx
@@ -0,0 +1,22 @@
+import { XCircleIcon } from "@heroicons/react/24/outline";
+
+interface CartProps{
+    setIsCartOpen: React.Dispatch<React.SetStateAction<boolean>>
+
+}
+
+export function Cart({setIsCartOpen}: CartProps){
+    return(
+        <div className="fixed bg-black/50 bottom-0 left-0 w-screen h-screen transition-all duration-200">
+            <div className="bg-white shadow-2xl py-6 fixed z-10 top-0 right-0 w-1/3 h-screen">
+                <div className="header flex justify-between text-slate-900 px-5">
+                    <h2 className="font-bold text-2xl">Seu carrinho</h2>
+                    <XCircleIcon onClick={() => setIsCartOpen(false)} className="hover:text-red-800 cursor-pointer duration-200" width={30}/>
+                </div>
+                <div className="grid grid-cols-1">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,7 +1,14 @@
+'use client'
+import React from "react";
+import { If } from "@/components/atoms";
+import { Cart } from "@/components/molecules";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useState } from "react";
 
 export function Header(){
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
     return (
         <header className="w-full fixed top-0 text-neutral-200 bg-slate-700 flex items-center justify-between p-4">
             <Link href={"/"}>
@@ -9,8 +16,11 @@ export function Header(){
             </Link>
             <div className="actions text-lg flex items-center gap-4">
                 <p className="hover:text-neutral-400 duration-200 cursor-pointer">Login</p>
-                <ShoppingCartIcon className="hover:text-neutral-400 duration-200 cursor-pointer" width={25}/>
+                <ShoppingCartIcon onClick={() => setIsCartOpen(!isCartOpen)} className="hover:text-neutral-400 duration-200 cursor-pointer" width={25}/>
             </div>
+            <If condition={isCartOpen}>
+                <Cart setIsCartOpen={setIsCartOpen}/>
+            </If>
         </header>
     )
 }
