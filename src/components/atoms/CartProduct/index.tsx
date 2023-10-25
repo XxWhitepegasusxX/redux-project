33ce3d91314baec7e6286c6e62c4cc7e313cdb07
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-10-25 15:41:36 -0300

    responsive cart adapted

--- src/components/atoms/CartProduct/index.tsx
+++ src/components/atoms/CartProduct/index.tsx
@@ -10,7 +10,7 @@ interface CartProductProps{
 export function CartProduct({id, name, price, imageUrl}: CartProductProps) {
     const [value, setValue] = useState(1)
     return (
-        <div className="container w-full h-auto grid grid-cols-4 items-center shadow-lg border border-neutral-400/50 rounded-lg p-4">
+        <div className="container w-full h-auto grid grid-cols-4 items-center shadow-lg border border-neutral-400/50 rounded-lg p-1 xs:p-2 sm:p-4">
             <img className="aspect-square w-20 object-cover rounded-xl" src={imageUrl} alt={name} />
             <h3 className="font-bold text-black text-center">{name}</h3>
             <p className="text-black">R${price},00</p>
--- src/components/molecules/Cart/index.tsx
+++ src/components/molecules/Cart/index.tsx
@@ -12,12 +12,12 @@ export function Cart({setIsCartOpen}: CartProps){
 
     return(
         <div className="fixed bg-black/50 bottom-0 left-0 w-screen h-screen transition-all duration-200">
-            <div className="bg-slate-100 shadow-2xl py-6 fixed z-10 top-0 right-0 w-1/3 h-screen">
+            <div className="bg-slate-100 shadow-2xl py-6 fixed z-10 top-0 right-0 w-full sm:w-2/3 lg:w-1/3 h-screen">
                 <div className="header flex justify-between text-slate-900 px-5">
                     <h2 className="font-bold text-2xl">Seu carrinho</h2>
                     <XCircleIcon onClick={() => setIsCartOpen(false)} className="hover:text-red-800 cursor-pointer duration-200" width={30}/>
                 </div>
-                <div className="grid grid-cols-1 mt-10">
+                <div className="grid grid-cols-1 mt-10 justify-center w-full p-0 xs:px-2">
                     <If condition={cartList}>
                         {cartList.map((product, index) => (
                             <CartProduct key={product.name + index} name={product.name} id={product.id} imageUrl={product.imageUrl} price={product.price} />
